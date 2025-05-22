60c52720fa2ab39ba21924bf6b59cc1f5d59be10
MauPos04 <mauriciop1palma@gmail.com>
2025-05-22 13:09:15 -0500

    Fix text formatting in Footer component and add favicon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,17 +13,16 @@ const Footer = () => {
             alt='grid'
             className='w-full h-full opacity-50'
             />
-        </div>
-        <div className='flex flex-col items-center'>
+        </div>        <div className='flex flex-col items-center'>
             <h1 className="heading lg:max-w--[45vw]">
                 Ready to take <span className='text-purple'>your</span> digital presence to the next level?
             </h1>
 
-            <p className="text-white-200 md:mt-10 my-5 text-center">Reach out to me today and let's discuss how can I help you achieve your goals</p>
+            <p className="text-white-200 md:mt-10 my-5 text-center">Reach out to me today and let&apos;s discuss how can I help you achieve your goals</p>
 
             <a href="mailto:mauriciop1palma@gmail.com" className="">
                 <MagicButton
-                title="Let's get in touch"
+                title="Let&apos;s get in touch"
                 icon={<FaLocationArrow />}
                 position='right'
                 />
